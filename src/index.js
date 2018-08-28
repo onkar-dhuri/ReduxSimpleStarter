e6fb1ed9e2f28b80b1fd2a4001450066a1a2d016
Onkar Dhuri <onkar.dhuri@gmail.com>
2018-08-28 16:02:52 +0530

    Added Youtube API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+ import React from 'react';
+ import ReactDOM from 'react-dom';
+ import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+//
+// import App from './components/app';
+// import reducers from './reducers';
+//
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+//
+// ReactDOM.render(
+//   <Provider store={createStoreWithMiddleware(reducers)}>
+//     <App />
+//   </Provider>
+//   , document.querySelector('.container'));
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const API_KEY = "AIzaSyC-_-0V82vMTwP9Cp73wboRLfD09B6n9HU";
+
+const App = () => {
+    return <div>Hi !!</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));
